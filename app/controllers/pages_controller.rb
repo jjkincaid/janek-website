ca956af3a02ab7199c2dd78f9d9fe0c080763e2c
Jesica Kincaid <jesica.j.kincaid@gmail.com>
2024-04-15 11:00:11 +0200

    initial routes & pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def overview
+  end
+
+  def apply
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ root to: "pages#home"
+
+  get "/about", to: "pages#about"
+
+  get "/overview", to: "pages#overview"
+
+  get "/apply", to: "pages#apply"
+
+  get "/contact", to: "pages#contact"
+
 end
